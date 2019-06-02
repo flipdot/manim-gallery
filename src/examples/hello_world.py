fdd5bc28c93f17699acb52d687cf8e805c88701a
Soeren Wegener <wegener92@gmail.com>
2019-06-02 14:07:59 +0200

    Basic hello world example

--- src/examples/hello_world.py
+++ src/examples/hello_world.py
@@ -4,4 +4,6 @@ from manimlib.imports import *
 class HelloWorld(Scene):
 
     def construct(self):
-        pass
+        hello = TexMobject('\\text{Hello manim!}')
+        self.play(Write(hello))
+        self.play(FadeOut(hello))
