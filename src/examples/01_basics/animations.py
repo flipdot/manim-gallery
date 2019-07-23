bc2bb5a0509df50ac9768af375c2490c2e20d435
Charly <charly.ahrendts@web.de>
2019-07-24 00:30:25 +0200

    new examples

---
+++ src/examples/01_basics/animations.py
@@ -0,0 +1,23 @@
+from manimlib.imports import *
+
+
+class ChangeShape(Scene):
+      def construct(self):
+          circle = Circle();
+          square = Square();
+
+          self.play(Transform(circle, square));
+          self.wait(1);
+          new_circle = Circle();
+          self.play(Transform(circle, new_circle));
+          self.wait(1);
+
+
+class Surround(Scene):
+    def construct(self):
+        circle = Circle();
+        square = Square();
+        circle.surround(square);
+        self.play(FadeIn(square));
+        self.play(GrowFromCenter(circle));
+        
