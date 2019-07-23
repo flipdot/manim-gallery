7d4353cf7ead291d81596f0715d58e8bef66b1e5
Soeren Wegener <wegener92@gmail.com>
2019-07-23 15:53:28 +0200

    Changed "Primitives" example

--- src/examples/01_basics/shapes.py
+++ src/examples/01_basics/shapes.py
@@ -4,7 +4,22 @@ from manimlib.imports import *
 class Primitives(Scene):
 
     def construct(self):
+        # TODO: put more complicated examples, e.g. parameters, in new example. Keep this one streight forward
+        # circle = Circle(
+        #     radius=4,
+        #     stroke_width=50,
+        #     stroke_color='#ffffff',
+        #     fill_color='#ff0000',
+        #     fill_opacity=1
+        # )
         circle = Circle()
-        self.play(Write(circle))
+        self.play(FadeIn(circle))
         self.play(FadeOut(circle))
 
+        square = Square()
+        self.play(FadeIn(square))
+        self.play(FadeOut(square))
+
+        rect = Rectangle()
+        self.play(FadeIn(rect))
+        self.play(FadeOut(rect))
