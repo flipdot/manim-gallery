a89f48cfb92dd6ee8aa3d6a82e8541d895eba4aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-29 16:34:17 +0000

    Deploy flipdot/manim-gallery to github.com/flipdot/manim-gallery.git:gh-pages

--- example_src/01_basics/attributes.py
+++ example_src/01_basics/attributes.py
@@ -2,26 +2,25 @@ from manimlib.imports import *
 
 
 class ColoringAndPosition(Scene):
-      def construct(self):
-          circle = Circle(color=RED)
-          square = Square(color='#0000FF')
-          triangle = Triangle(color=GREEN)
-          square.move_to(RIGHT)
-          circle.move_to(LEFT)
-          triangle.move_to(2*UP)
-          large_circle=Circle(radius=3,color=WHITE)
-          self.play(FadeIn(square))
-          self.play(FadeIn(circle))
-          self.play(FadeIn(triangle))  
-          self.wait(1)
+    def construct(self):
+        circle = Circle(color=RED)
+        square = Square(color='#0000FF')
+        triangle = Triangle(color=GREEN)
+        square.move_to(RIGHT)
+        circle.move_to(LEFT)
+        triangle.move_to(2 * UP)
+        self.play(FadeIn(square))
+        self.play(FadeIn(circle))
+        self.play(FadeIn(triangle))
+        self.wait(1)
 
 
 class Size(Scene):
     def construct(self):
         circle = Circle()
-        medium_circle=Circle(radius=2, color ='#0000FF')
-        large_circle=Circle(radius=3,color = WHITE)
-        
+        medium_circle = Circle(radius=2, color='#0000FF')
+        large_circle = Circle(radius=3, color=WHITE)
+
         self.play(ShowCreation(circle))
         self.play(ShowCreation(medium_circle))
         self.play(ShowCreation(large_circle))
