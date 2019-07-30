6788020ea74e4243b8ddf7c208f4ea564ee23e97
Soeren Wegener <wegener92@gmail.com>
2019-07-30 16:14:39 +0200

    Play grid animation faster for decreased render time

--- src/examples/01_basics/grid.py
+++ src/examples/01_basics/grid.py
@@ -10,9 +10,9 @@ class Grid(Scene):
     def construct(self):
         for x in range(-7, 8):
             for y in range(-5, 6):
-                annotation = TexMobject(f'P({x}, {y}, 0))', height=1.5, fill_opacity=.5)
+                annotation = TexMobject(f'P({x}, {y}, 0)', height=1.5, fill_opacity=.5)
                 self.add(annotation)
                 dot = Dot(point=(x, y, 0))
                 self.add(dot)
-                self.wait(.5)
+                self.wait(.05)
                 self.remove(annotation)
