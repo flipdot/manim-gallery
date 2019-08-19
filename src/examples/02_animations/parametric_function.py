14aef8685f4db771ae6132d0bb249b74757a22bd
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2019-08-19 18:28:03 +0200

    Labled the function
    Added a function that makes a Lissajous_curve_func:
    https://en.wikipedia.org/wiki/Lissajous_curve

--- src/examples/02_animations/parametric_function.py
+++ src/examples/02_animations/parametric_function.py
@@ -1,14 +1,14 @@
 from manimlib.imports import *
 
 
-def i_need_a_name(t):
+def Lissajous_curve_func(t):
     return np.array((np.sin(2 * t), np.sin(3 * t), 0))
 
 
 class ParamFunc(Scene):
 
     def construct(self):
-        func = ParametricFunction(i_need_a_name, t_max=TAU, fill_opacity=0)
+        func = ParametricFunction(Lissajous_curve_func, t_max=TAU, fill_opacity=0)
         dot = Dot()
         self.add(dot)
         self.add(func)
