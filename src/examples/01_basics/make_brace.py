8bd8c596efb2f0f78677ce1b2b0707bbbc6613cd
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2019-08-19 20:22:59 +0200

    Make a brace and text

---
+++ src/examples/01_basics/make_brace.py
@@ -0,0 +1,13 @@
+from manimlib.imports import *
+
+ class MakeBrace(Scene):
+    def construct(self):
+        dot = Dot([0,0,0])
+        dot2= Dot([2,1,0])
+        line = Line(dot,dot2)
+        b=Brace(VGroup(dot,dot2),UP)
+        eq_text = b.get_tex("x-x_1")
+
+         self.add(dot,dot2)
+        self.add(line,b, eq_text)
+        self.wait(0.1)
