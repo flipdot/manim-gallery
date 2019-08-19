d9272305982baab50b6d92024301f26a6dbfe11c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-19 17:52:20 +0000

    Deploy flipdot/manim-gallery to github.com/flipdot/manim-gallery.git:gh-pages

---
+++ example_src/02_animations/parametric_function.py
@@ -0,0 +1,15 @@
+from manimlib.imports import *
+
+
+def lissajous_curve_func(t):
+    return np.array((np.sin(2 * t), np.sin(3 * t), 0))
+
+
+class ParamFunc(Scene):
+
+    def construct(self):
+        func = ParametricFunction(lissajous_curve_func, t_max=TAU, fill_opacity=0)
+        dot = Dot()
+        self.add(dot)
+        self.add(func)
+        self.wait(0.1)
