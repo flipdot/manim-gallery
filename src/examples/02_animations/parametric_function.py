326b542084f83262d62ee603de0fdef4d72989d3
Soeren Wegener <wegener92@gmail.com>
2019-08-19 19:21:14 +0200

    Change function name to lowercase

--- src/examples/02_animations/parametric_function.py
+++ src/examples/02_animations/parametric_function.py
@@ -1,14 +1,14 @@
 from manimlib.imports import *
 
 
-def Lissajous_curve_func(t):
+def lissajous_curve_func(t):
     return np.array((np.sin(2 * t), np.sin(3 * t), 0))
 
 
 class ParamFunc(Scene):
 
     def construct(self):
-        func = ParametricFunction(Lissajous_curve_func, t_max=TAU, fill_opacity=0)
+        func = ParametricFunction(lissajous_curve_func, t_max=TAU, fill_opacity=0)
         dot = Dot()
         self.add(dot)
         self.add(func)
